21e1a7155d154a795636b1e7a0389c603017aa29
BenSibley <ben@competethemes.com>
2016-03-25 10:50:47 -0400

    cross browser improvements and page markup never finished

--- content-page.php
+++ content-page.php
@@ -1,10 +1,10 @@
 <div <?php post_class(); ?>>
 	<?php do_action( 'page_before' ); ?>
 	<article>
-		<?php ct_shift_featured_image(); ?>
 		<div class='post-header'>
 			<h1 class='post-title'><?php the_title(); ?></h1>
 		</div>
+		<?php ct_shift_featured_image(); ?>
 		<div class="post-content">
 			<?php the_content(); ?>
 			<?php wp_link_pages( array(
