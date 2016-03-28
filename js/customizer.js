52412fb743ae5315c5383440c7a2b8f8c445f6c7
BenSibley <ben@competethemes.com>
2016-03-28 13:43:52 -0400

    layout customizer ad better positioned

--- js/customizer.js
+++ js/customizer.js
@@ -1,3 +1,4 @@
 jQuery(document).ready(function($){
     $('#accordion-section-shift_display').find('.customize-section-description').appendTo('#customize-control-display_post_comments');
+    $('#accordion-section-shift_layout').find('.customize-section-description').appendTo('#customize-control-layout');
 });
