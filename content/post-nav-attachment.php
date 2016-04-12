3869e940ca27a3dc3cf05c70ac126e79d7bac473
BenSibley <ben@competethemes.com>
2016-04-12 08:07:36 -0400

    slight change to markup/style for attachment navigation

--- content/post-nav-attachment.php
+++ content/post-nav-attachment.php
@@ -1,8 +1,8 @@
 <nav class="further-reading">
 	<div class="previous">
-		<span><?php previous_image_link( false, __( 'Previous Image', 'shift' ) ); ?></span>
+		<?php previous_image_link( false, __( 'Previous Image', 'shift' ) ); ?>
 	</div>
 	<div class="next">
-		<span><?php next_image_link( false, __( 'Next Image', 'shift' ) ); ?></span>
+		<?php next_image_link( false, __( 'Next Image', 'shift' ) ); ?>
 	</div>
 </nav>
