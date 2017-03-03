ff411b64dedddda9146288a2ddc51aef5a4c03dc
BenSibley <ben@competethemes.com>
2017-03-03 13:28:09 -0500

    more

--- content/comments-link.php
+++ content/comments-link.php
@@ -2,10 +2,10 @@
 	<i class="fa fa-comment" aria-hidden="true"></i>
 	<?php
 	if ( ! comments_open() && get_comments_number() < 1 ) :
-		comments_number( __( 'Comments closed', 'shift' ), __( '1 Comment', 'shift' ), _x( '% Comments', 'noun: 5 comments', 'cele' ) );
+		comments_number( __( 'Comments closed', 'shift' ), __( '1 Comment', 'shift' ), _x( '% Comments', 'noun: 5 comments', 'shift' ) );
 	else :
 		echo '<a href="' . esc_url( get_comments_link() ) . '">';
-		comments_number( __( 'Leave a Comment', 'shift' ), __( '1 Comment', 'shift' ), _x( '% Comments', 'noun: 5 comments', 'cele' ) );
+		comments_number( __( 'Leave a Comment', 'shift' ), __( '1 Comment', 'shift' ), _x( '% Comments', 'noun: 5 comments', 'shift' ) );
 		echo '</a>';
 	endif;
 	?>
