13906aa7dceae2f57d456050c9889a9183480e58
BenSibley <ben@competethemes.com>
2017-05-30 11:21:54 -0400

    stopped adding ellipsis after custom excerpts

--- functions.php
+++ functions.php
@@ -155,7 +155,7 @@ if ( ! function_exists( 'ct_shift_remove_comments_notes_after' ) ) {
 add_action( 'comment_form_defaults', 'ct_shift_remove_comments_notes_after' );
 
 if ( ! function_exists( 'ct_shift_filter_read_more_link' ) ) {
-	function ct_shift_filter_read_more_link() {
+	function ct_shift_filter_read_more_link( $custom = false ) {
 		global $post;
 		$ismore             = strpos( $post->post_content, '<!--more-->' );
 		$read_more_text     = get_theme_mod( 'read_more_text' );
@@ -164,7 +164,7 @@ if ( ! function_exists( 'ct_shift_filter_read_more_link' ) ) {
 		$output = '';
 
 		// add ellipsis for automatic excerpts
-		if ( empty( $ismore ) ) {
+		if ( empty( $ismore ) && $custom !== true  ) {
 			$output .= $excerpt_more;
 		}
 		// Because i18n text cannot be stored in a variable
@@ -184,7 +184,7 @@ if ( ! function_exists( 'ct_shift_filter_manual_excerpts' ) ) {
 	function ct_shift_filter_manual_excerpts( $excerpt ) {
 		$excerpt_more = '';
 		if ( has_excerpt() ) {
-			$excerpt_more = ct_shift_filter_read_more_link();
+			$excerpt_more = ct_shift_filter_read_more_link( true );
 		}
 		return $excerpt . $excerpt_more;
 	}
