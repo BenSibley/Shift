ebf827d6f07781b219b61adacd70bf2637f62832
BenSibley <ben@competethemes.com>
2017-08-11 11:55:39 -0400

    switched to GIF for Pro ad

--- inc/customizer.php
+++ inc/customizer.php
@@ -25,7 +25,7 @@ function ct_shift_add_customizer_content( $wp_customize ) {
 	class ct_shift_pro_ad extends WP_Customize_Control {
 		public function render_content() {
 			$link = 'https://www.competethemes.com/shift-pro/';
-			echo "<a href='" . $link . "' target='_blank'><img src='" . get_template_directory_uri() . "/assets/images/shift-pro.png' srcset='" . get_template_directory_uri() . "/assets/images/shift-pro-2x.png 2x' /></a>";
+			echo "<a href='" . $link . "' target='_blank'><img src='" . get_template_directory_uri() . "/assets/images/shift-pro.gif' /></a>";
 			echo "<p class='bold'>" . sprintf( __('<a target="_blank" href="%1$s">%2$s Pro</a> is the plugin that makes advanced customization simple - and fun too!', 'shift'), $link, wp_get_theme( get_template() )) . "</p>";
 			echo "<p>" . sprintf( __('%1$s Pro adds the following features to %1$s:', 'shift'), wp_get_theme( get_template() ) ) . "</p>";
 			echo "<ul>
