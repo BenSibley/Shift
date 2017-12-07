91e6b9a73c173e928c85f63cb87325e71c045f0d
BenSibley <ben@competethemes.com>
2017-12-07 11:03:55 -0500

    fixed layout thumbnails in Customizer

--- js/customizer.js
+++ js/customizer.js
@@ -15,11 +15,11 @@ jQuery(document).ready(function($){
 
         // add the appropriate image to each label
         layoutInputs.each( function() {
-            $(this).parent().css('background-image', 'url("' + ct_shift_customizer.templateDir + '/assets/images/' + $(this).val() + '.png")');
+            $(this).next().css('background-image', 'url("' + ct_shift_customizer.templateDir + '/assets/images/' + $(this).val() + '.png")');
 
             // add initial 'selected' class
             if ($(this).prop('checked')) {
-                $(this).parent().addClass('selected');
+                $(this).next().addClass('selected');
             }
         });
 
@@ -33,11 +33,11 @@ jQuery(document).ready(function($){
     function addSelectedLayoutClass(inputs, target) {
 
         // remove 'selected' class from all labels
-        inputs.parent().removeClass('selected');
+        inputs.next().removeClass('selected');
 
         // apply 'selected' class to :checked input
         if (target.prop('checked')) {
-            target.parent().addClass('selected');
+            target.next().addClass('selected');
         }
     }
 });
