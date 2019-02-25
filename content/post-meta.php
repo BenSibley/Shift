4e42627cfbf43bb8c7657d85059fd338c92647da
BenSibley <ben@competethemes.com>
2019-02-25 15:32:40 -0500

    WP 5.1 post date fix

--- content/post-meta.php
+++ content/post-meta.php
@@ -18,7 +18,7 @@ if ( $author_display == 'hide' && $date_display == 'hide' && $comments_display =
 	<?php if ( $date_display != 'hide' ) : ?>
 		<div class="date">
 			<i class="fas fa-calendar-alt" aria-hidden="true"></i>
-			<a class='date' href="<?php echo esc_url( get_month_link( get_the_date( 'Y' ), get_the_date( 'n' ) ) ); ?>"><?php echo date_i18n( get_option( 'date_format' ), strtotime( get_the_date( 'r' ) ) ); ?></a>
+			<a class='date' href="<?php echo esc_url( get_month_link( get_the_date( 'Y' ), get_the_date( 'n' ) ) ); ?>"><?php echo date_i18n( get_option( 'date_format' ), strtotime( get_the_date( 'c' ) ) ); ?></a>
 		</div>
 	<?php endif; ?>
 	<?php if ( $comments_display != 'hide' ) : ?>
