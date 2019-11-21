c791c338db732bd9290bd14244643f3ead858561
BenSibley <ben@competethemes.com>
2019-11-21 13:42:04 -0500

    fixed PHP error notice

--- theme-options.php
+++ theme-options.php
@@ -1,7 +1,13 @@
 <?php
 
 function ct_shift_register_theme_page() {
-	add_theme_page( sprintf( esc_html__( '%s Dashboard', 'shift' ), wp_get_theme() ), sprintf( esc_html__( '%s Dashboard', 'shift' ), wp_get_theme() ), 'edit_theme_options', 'shift-options', 'ct_shift_options_content', 'ct_shift_options_content' );
+	add_theme_page( 
+		sprintf( esc_html__( '%s Dashboard', 'shift' ), wp_get_theme() ), 
+		sprintf( esc_html__( '%s Dashboard', 'shift' ), wp_get_theme() ), 
+		'edit_theme_options', 
+		'shift-options', 
+		'ct_shift_options_content'
+	);
 }
 add_action( 'admin_menu', 'ct_shift_register_theme_page' );
 
